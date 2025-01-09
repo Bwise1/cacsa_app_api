d96c8d8d867972d733f1d3973ae9cdf12ed34933
Bwise1 <oguntoyebenjamin2@gmail.com>
2025-01-09 15:18:51 +0200

    cron to server

--- cronjobs.js
+++ cronjobs.js
@@ -1,65 +1,73 @@
-const cron = require("node-cron");
-const { subscriptionsCollection } = require("./utils/firebase/index");
-const SubscriptionModel = require("./models/SubscriptionModel");
+if (require.main === module) {
+  const cron = require("node-cron");
+  const { subscriptionsCollection } = require("./utils/firebase/index");
+  const SubscriptionModel = require("./models/SubscriptionModel");
 
-async function expireSubscriptions() {
-  try {
-    const rows = await SubscriptionModel.getExpiredSubscriptions();
-    console.log("Rows:", rows);
+  async function expireSubscriptions() {
+    try {
+      const rows = await SubscriptionModel.getExpiredSubscriptions();
+      console.log("Rows:", rows);
 
-    if (rows.length === 0) {
-      console.log("No expired subscriptions");
-      return;
-    }
-
-    await SubscriptionModel.updateExpiredSubscriptions();
-
-    for (const row of rows) {
-      const docRef = subscriptionsCollection.doc(row.uid);
-      try {
-        await docRef.delete();
-      } catch (error) {
-        console.error(`Error deleting document with uid ${row.uid}:`, error);
+      if (rows.length === 0) {
+        console.log("No expired subscriptions");
+        return;
       }
-    }
 
-    console.log("Subscriptions updated to expired and removed from Firestore");
-  } catch (error) {
-    console.error(
-      "Error updating subscriptions to expired and removing them from Firestore",
-      error
-    );
-  }
-}
+      await SubscriptionModel.updateExpiredSubscriptions();
 
-async function deleteActiveSubscriptions() {
-  try {
-    const rows = await SubscriptionModel.getAllActiveSubscriptions();
-    console.log("Rows:", rows);
-
-    if (rows.length === 0) {
-      console.log("No expired subscriptions");
-      return;
-    }
-
-    for (const row of rows) {
-      const docRef = subscriptionsCollection.doc(row.uid);
-      try {
-        await docRef.delete();
-      } catch (error) {
-        console.error(`Error deleting document with uid ${row.uid}:`, error);
+      for (const row of rows) {
+        const docRef = subscriptionsCollection.doc(row.uid);
+        try {
+          await docRef.delete();
+        } catch (error) {
+          console.error(`Error deleting document with uid ${row.uid}:`, error);
+        }
       }
+
+      console.log(
+        "Subscriptions updated to expired and removed from Firestore"
+      );
+    } catch (error) {
+      console.error(
+        "Error updating subscriptions to expired and removing them from Firestore",
+        error
+      );
     }
-
-    console.log("Subscriptions updated to expired and removed from Firestore");
-  } catch (error) {
-    console.error(
-      "Error updating subscriptions to expired and removing them from Firestore",
-      error
-    );
   }
-}
 
-// Schedule the function to run every day at 00:00
-cron.schedule("* * * * *", expireSubscriptions);
-cron.schedule("* * * * *", deleteActiveSubscriptions);
+  async function deleteActiveSubscriptions() {
+    try {
+      const rows = await SubscriptionModel.getAllActiveSubscriptions();
+      console.log("Rows:", rows);
+
+      if (rows.length === 0) {
+        console.log("No expired subscriptions");
+        return;
+      }
+
+      for (const row of rows) {
+        const docRef = subscriptionsCollection.doc(row.uid);
+        try {
+          await docRef.delete();
+        } catch (error) {
+          console.error(`Error deleting document with uid ${row.uid}:`, error);
+        }
+      }
+
+      console.log(
+        "Subscriptions updated to expired and removed from Firestore"
+      );
+    } catch (error) {
+      console.error(
+        "Error updating subscriptions to expired and removing them from Firestore",
+        error
+      );
+    }
+  }
+
+  // Execute both tasks sequentially when called
+  (async () => {
+    await expireSubscriptions();
+    await deleteActiveSubscriptions();
+  })();
+}
